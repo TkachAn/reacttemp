1789a099bc3b7fc222113381e80f1b445abbbf0e
TkachAn <4ndrey.tkachenko@gmail.com>
2024-02-07 23:34:21 +0200

    menu

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import NavBar from './components/navbar/navbar';
-import './index.css';
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { LayOut } from './components/layout/layout';
+import { HomePage } from './pages/homePage';
+import { ContactsPage } from './pages/contactsPage';
+import { AboutUsPage } from './pages/aboutPage';
+import { NotFoundPage } from './pages/notFoundPage';
 
 function App() {
 	return (
-		<div>
-			<NavBar />
-		</div>
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<LayOut />}>
+					<Route index element={<HomePage />} />
+					<Route path='/contacts' element={<ContactsPage />} />
+					<Route path='/about' element={<AboutUsPage />} />
+					<Route path='*' element={<NotFoundPage />} />
+				</Route>
+			</Routes>
+		</BrowserRouter>
 	);
 }
 
