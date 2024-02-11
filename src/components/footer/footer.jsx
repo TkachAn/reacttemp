19516a7e012bd6531bba345f3bca8bfd8911b55e
TkachAn <4ndrey.tkachenko@gmail.com>
2024-02-11 21:52:51 +0200

    burger

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -2,18 +2,38 @@ import s from "./foo.module.css";
 
 export const Footer = () => {
   return (
-    <div className={s.wrapper}>
+    <footer className={s.wrapper}>
       <div className="container">
-        <div className={s.content}>
-          <h1>Footer</h1>
-          <p className={s.text}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur
-            pariatur veritatis aspernatur, recusandae dolorum tempora dolor, ab
-            expedita at esse dolorem, fugit amet quibusdam eaque non soluta est
-            provident tempore!
-          </p>
+        <div className={s.flex}>
+          <div className={s.box}>
+            <h2>Footer</h2>
+            <p className={s.text}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Consequatur pariatur veritatis aspernatur, recusandae dolorum
+              tempora dolor, ab expedita at esse dolorem, fugit amet quibusdam
+              eaque non soluta est provident tempore!
+            </p>
+          </div>
+					<div className={s.box}>
+            <h2>Footer</h2>
+            <p className={s.text}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Consequatur pariatur veritatis aspernatur, recusandae dolorum
+              tempora dolor, ab expedita at esse dolorem, fugit amet quibusdam
+              eaque non soluta est provident tempore!
+            </p>
+          </div>
+					<div className={s.box}>
+            <h2>Footer</h2>
+            <p className={s.text}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Consequatur pariatur veritatis aspernatur, recusandae dolorum
+              tempora dolor, ab expedita at esse dolorem, fugit amet quibusdam
+              eaque non soluta est provident tempore!
+            </p>
+          </div>
         </div>
       </div>
-    </div>
+    </footer>
   );
 };
