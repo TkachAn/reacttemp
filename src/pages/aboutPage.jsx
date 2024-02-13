1706e627ab0a24fbfc8726aaa39243b04a560167
TkachAn <4ndrey.tkachenko@gmail.com>
2024-02-13 23:24:41 +0200

    about

--- src/pages/aboutPage.jsx
+++ src/pages/aboutPage.jsx
@@ -1,19 +1,7 @@
-import React, { useState } from "react";
-import { NavLink } from "react-router-dom";
-import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
-
 import s from "./page.module.css";
-import { Dialog } from "@headlessui/react";
-
-const links = [
-  { link: "/", title: "home", icon: "" },
-  { link: "/about", title: "about us", icon: "" },
-  { link: "/contacts", title: "contacts", icon: "" },
-  { link: "/download", title: "download", icon: "" },
-];
 
 export const AboutUsPage = () => {
-  const [isOpen, setOpen] = useState(false);
+ 
   return (
     <div className={s.wrapper}>
       <div className="container">
